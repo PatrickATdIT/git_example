2d7bc13b61fc9a555cd8e8ee49210fc32284e012
PatrickATdIT <atdit@wurstbox.com>
2024-03-09 13:31:12 +0100

    Implementieren des Hauptfeatures     Hinzufügen Javadocs für Main-Methode     Implementierung der Main-Methode     Anlegen der Main-Klasse

---
+++ src/main/java/de/hwglu/atdit_2024/Main.java
@@ -0,0 +1,17 @@
+package de.hwglu.atdit_2024;
+
+public class Main {
+    /**
+     * Dies ist der Einstiegspunkt in unsere Applikation.<br/>
+     * Die Methode macht folgendes:
+     * <ol>
+     *   <li>Hallo Welt schreiben</li>
+     *   <li>Einen schönen Tag wünschen</li></ol>
+     *
+     * @param args Kommandozeilenparameter; werden nicht genutzt
+     */
+    public static void main(String[] args) {
+        System.out.println("Hallo, Welt");
+        System.out.println("Hab einen schönen Tag");
+    }
+}
